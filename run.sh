d9204ddad8a704b6fd932ba44108036e48133f11
蔡子文 <cai.ziwen@qianka.com>
2017-12-05 16:33:58 +0800

    update shell

--- run.sh
+++ run.sh
@@ -2,3 +2,15 @@
 passwd = $1
 echo "password: ${passwd}"
 yes|sudo apt-get install shadowsocks
+sudo bash -c "echo \
+'{
+    \"server\": \"0.0.0.0\",
+    \"server_port\": 22,
+    \"password\": \"${passwd}\",
+    \"timeout\": 360,
+    \"method\": \"aes-256-cfb\",
+    \"fast_open\": true,
+    \"workers\": 2
+}' > /etc/shadowsocks/config.json
+"
+sudo service shadowsocks restart
