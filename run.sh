5960705d0f1f2ffc78e51068a9068d7df6244827
wwwcevon <804625920@qq.com>
2019-09-17 14:29:49 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -l
 passwd=$1
+if [ -z "$passwd"];
+then
+echo "need password"
+echo "bash run.sh {your password}"
+exit
+fi
 echo "password: ${passwd}"
+if apt-cache pkgnames | grep -q shadowsocks
+then
+echo "shadowsocks has been installed"
+else
 yes|sudo apt-get install shadowsocks
+fi
 sudo bash -c "echo \"127.0.0.1 `hostname`\" >> /etc/hosts"
 sudo bash -c "echo \
 '{
@@ -14,4 +25,5 @@ sudo bash -c "echo \
     \"workers\": 2
 }' > /etc/shadowsocks/config.json
 "
+killall ssserver
 nohup ssserver -c /etc/shadowsocks/config.json & 2>&1 > /dev/null
